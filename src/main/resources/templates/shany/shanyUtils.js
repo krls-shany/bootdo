1efe395685e011905f91d7cf97eeeb7ea1c2cb53
shany <597796728@qq.com>
2019-08-02 17:22:37 +0800

    2019-08-02

--- src/main/resources/templates/shany/shanyUtils.js
+++ src/main/resources/templates/shany/shanyUtils.js
@@ -1,21 +1,21 @@
 //default parameter
 
-var cycleTime;          //定时器
-var status              //js状态管理
+var cycleTime; //定时器
+var status //js状态管理
 
 /**
  * 默认执行的function
  * 加入拓展机制
  */
 function init() {
-    
+
 }
 
 function destroy() {
-    cycleTime=null;
-    timeListen={};
-    jsonAnalysis={};
-    config={};
+    cycleTime = null;
+    timeListen = {};
+    jsonAnalysis = {};
+    config = {};
     console.clear();
 }
 
@@ -25,22 +25,23 @@ function destroy() {
  */
 function timeListen(json) {
     //对传入的json判断解析为
-    
-    cycleTime = setInterval(()=>jsonAnalysis(json,'cycleTime',()=>{
+
+    cycleTime = setInterval(() => jsonAnalysis(json, 'cycleTime', () => {
         //--------默认的定时器函数
-        
+
+        //检查状态和程序运行情况
         check();
 
         //判断状态
-        if(status=='stop'){
+        if (status == 'stop') {
             stop();
-        }else if(status=='destroy'){
+        } else if (status == 'destroy') {
             stop(); //待定
             destroy();
         }
 
 
-    }),1000);
+    }), 1000);
 
 }
 
@@ -49,8 +50,8 @@ function timeListen(json) {
  * @param {json} json 
  */
 function check(json) {
-    if(1==0){
-        status='destroy';
+    if (1 == 0) {
+        status = 'destroy';
     }
 }
 
@@ -60,15 +61,15 @@ function check(json) {
  * @param {*} info  要请求的内容
  * @param {*} data  默认的返回数据，在json中查找不到或者其他错误时返回该值
  */
-function jsonAnalysis(json,info,data) {
+function jsonAnalysis(json, info, data) {
     //当传入的json，info数据内容或格式错误时
-    if(!isNull(info)||!isNull(json)||!isNull(toJson(json))){
+    if (!isNull(info) || !isNull(json) || !isNull(toJson(json))) {
         return data;
     }
-    json=toJson(json);
-    var result=eval('json.'+info);
+    json = toJson(json);
+    var result = eval('json.' + info);
     //如果json中包含吃数据，则返回
-    if(isNull(result)){
+    if (isNull(result)) {
         return result;
     }
     //默认返回data
@@ -80,7 +81,7 @@ function jsonAnalysis(json,info,data) {
  * @param {*} json  json格式的配置文件
  */
 function config(json) {
-    
+
 }
 
 /**
@@ -89,7 +90,7 @@ function config(json) {
  * @param {*} params 
  */
 function stop(params) {
-    
+
 }
 
 /**
@@ -97,16 +98,16 @@ function stop(params) {
  * @param {*} data 
  */
 function isNull(data) {
-    if(data==null){
-        console.log("[INFO]:isNull() data:"+data+" is null");
+    if (data == null) {
+        console.log("[INFO]:isNull() data:" + data + " is null");
         return false;
     }
-    if(data==""){
-        console.log("[INFO]:isNull() data:"+data+" is ''");
+    if (data == "") {
+        console.log("[INFO]:isNull() data:" + data + " is ''");
         return false;
     }
-    if(data==undefined){
-        console.log("[INFO]:isNull() data:"+data+" is undefined");
+    if (data == undefined) {
+        console.log("[INFO]:isNull() data:" + data + " is undefined");
         return false;
     }
     return true;
@@ -116,17 +117,15 @@ function isNull(data) {
  * @param {*} json 
  */
 function toJson(json) {
-    if(!isNull(json))
+    if (!isNull(json))
         return null;
     if (typeof json == 'string') {
         try {
-            json=JSON.parse(json);
+            json = JSON.parse(json);
             return json;
-        } catch(e) {
-            console.log('[ERROR]:toJson() json:'+json+'转化异常');
+        } catch (e) {
+            console.log('[ERROR]:toJson() json:' + json + '转化异常');
         }
     }
     return json;
 }
-
-
